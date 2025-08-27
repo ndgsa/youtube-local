306b9a4edd9ef267e4b91427947bf2733ee510a7
ndgsa <59797540+ndgsa@users.noreply.github.com>
2025-08-27 16:39:04 +0300

    some fix after rename repo

--- run_innertube-client1.bat
+++ run_innertube-client1.bat
@@ -17,18 +17,18 @@ SET youtube_local_server_path=%script_dir%innertube-client1\
 
 ::  So this bat file can be called from a different working directory.
 ::  %~dp0 is the directory with this bat file.
-cd /d %script_dir%
+cd /d "%script_dir%"
 
 ::  This is so brotli and gevent search in the python directory for the
 ::  visual studio c++ runtime dlls
-IF EXIST %python_exec_path% (
+IF EXIST "%python_exec_path%" (
 	set PATH="%python_exec_path%;%PATH%"
 	set py_exe_loc="%python_exec_path%python.exe"
 ) ELSE (set py_exe_loc="python")
 
-cd /d %youtube_local_server_path%
+cd /d "%youtube_local_server_path%"
 
-%py_exe_loc% "%youtube_local_server_path%server.py"s
+%py_exe_loc% "%youtube_local_server_path%server.py"
 echo Press any key to quit...
 PAUSE > nul
 
