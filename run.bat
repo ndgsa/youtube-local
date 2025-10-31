77fb973491bad432b85b210663bbeb8cb67a13fb
ndgsa <59797540+ndgsa@users.noreply.github.com>
2025-10-31 21:45:30 +0200

    feature: change structure of project
    -Remove unusable files
    -Move files into another folder
    -Load templates, static folders from assets folder
    -Rename and update .bat launcher
    -...

--- run.bat
+++ run.bat
@@ -1,18 +1,34 @@
+:: @echo off
+:: setlocal
+:: cd /d "youtube-local\" && python "server.py"
+:: echo Press any key to quit...
+:: PAUSE > nul
+
 @echo off
 
-REM  https://stackoverflow.com/a/25719250
-REM  setlocal makes sure changing directory only applies inside this bat file,
-REM  and not in the command shell.
-setlocal
+::  https://stackoverflow.com/a/25719250
+::  setlocal makes sure changing directory only applies inside this bat file,
+::  and not in the command shell.
+SETLOCAL
 
-REM  So this bat file can be called from a different working directory.
-REM  %~dp0 is the directory with this bat file.
-cd /d "%~dp0"
+SET script_dir=%~dp0
+SET python_exec_path=%script_dir%python\
+SET youtube_local_server_path=%script_dir%\
 
-REM  This is so brotli and gevent search in the python directory for the
-REM  visual studio c++ runtime dlls
-set PATH=.\python;%PATH%
+::  So this bat file can be called from a different working directory.
+::  %~dp0 is the directory with this bat file.
+cd /d "%script_dir%"
 
-.\python\python.exe -I .\server.py
+::  This is so brotli and gevent search in the python directory for the
+::  visual studio c++ runtime dlls
+IF EXIST "%python_exec_path%" (
+	set PATH="%python_exec_path%;%PATH%"
+	set py_exe_loc="%python_exec_path%python.exe"
+) ELSE (set py_exe_loc="python")
+
+cd /d "%youtube_local_server_path%"
+
+%py_exe_loc% -I "%youtube_local_server_path%server.py"
 echo Press any key to quit...
 PAUSE > nul
+
