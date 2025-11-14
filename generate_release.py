369bd64f5a7f7dbe99f2bfa15114bb0341dfc2b8
ndgsa <59797540+ndgsa@users.noreply.github.com>
2025-11-14 18:35:37 +0200

    signature_solvers: add signature solver for n/sig decryption based on extracted functions by youtubei.js
    - Use youtubei.js lib to extract n/sig functions
    - Extracting of functions will require one of javascript runtime [ 'node' ]
    - Use dukpy/nodejs to solve n/sig decryption based on extracted functions by youtubei.js
    
    Credits to `@LuanRT` team for `youtubei.js` package.
    Credits to `@alive4ever` for helping.

--- generate_release.py
+++ generate_release.py
@@ -299,6 +299,14 @@ log('Copying yt_solver js files')
 shutil.move(yt_solver_lib_name, r'./youtube-local/assets/other/js/ejs/' + yt_solver_lib_name)
 shutil.move(yt_solver_core_name, r'./youtube-local/assets/other/js/ejs/' + yt_solver_core_name)
 
+os.makedirs('./youtube-local/assets/other/js/youtubei/', exist_ok=True)
+youtubei_version = '17.0.1'
+youtubei_lib_url = f"https://cdn.jsdelivr.net/npm/youtubei.js@{youtubei_version}/bundle/browser.min.js"
+youtubei_lib_name = 'youtubei_' + youtubei_version.replace('.', '') + '.js'
+download_if_not_exists(youtubei_lib_name, youtubei_lib_url)
+log('Copying youtubei file')
+shutil.move(youtubei_lib_name, r'./youtube-local/assets/other/js/youtubei/' + youtubei_lib_name)
+
 # ----------- Create release zip -----------
 output_filename = release_tag + '-' + suffix + '-' + bitness + '.zip'
 if os.path.exists('./' + output_filename):
