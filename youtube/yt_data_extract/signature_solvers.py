1ec42fc4381ca5ecdd91e0b9096635c1ad79db7f
ndgsa <59797540+ndgsa@users.noreply.github.com>
2025-11-14 18:31:30 +0200

    signature_solvers: use js runtime
    Add feature to launch js runtime.

--- youtube/yt_data_extract/signature_solvers.py
+++ youtube/yt_data_extract/signature_solvers.py
@@ -6,6 +6,8 @@ import os
 import sys
 import json
 import re
+import shutil
+import subprocess
 
 
 ################################################################################ mine
@@ -16,6 +18,130 @@ def signature_solver(s, info):
 
 def requires_decryption(info):
     return ('formats' in info) and info['formats'] and info['formats'][0]['s']
+
+
+class Popen(subprocess.Popen):
+    if sys.platform == 'win32':
+        _startupinfo = subprocess.STARTUPINFO()
+        _startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    else:
+        _startupinfo = None
+
+    def __init__(self, args, *remaining, env=None, text=False, shell=False, **kwargs):
+        if env is None:
+            env = os.environ.copy()
+        super().__init__(args, *remaining, env=env, shell=shell, **kwargs, startupinfo=self._startupinfo)
+
+    def communicate_or_kill(self, *args, **kwargs):
+        try:
+            return self.communicate(*args, **kwargs)
+        except BaseException:  # Including KeyboardInterrupt
+            self.kill(timeout=None)
+            raise
+
+    def kill(self, *, timeout=0):
+        print('kill')
+        super().kill()
+        if timeout != 0:
+            self.wait(timeout=timeout)
+
+    @classmethod
+    def run(cls, *args, timeout=None, **kwargs):
+        with cls(*args, **kwargs) as proc:
+            default = '' if proc.__text_mode else b''
+            stdout, stderr = proc.communicate_or_kill(timeout=timeout)
+            return stdout or default, stderr or default, proc.returncode
+
+def find_executable(executable_name):
+    """Search for an executable in the system path"""
+    # print(os.path.realpath(shutil.which('node', mode=os.F_OK | os.X_OK, path=os.environ["PATH"])))
+    executable_path_list = []
+    js_dir = os.path.join(settings.other_dir, 'js')
+    if not os.path.isdir(js_dir): os.makedirs(js_dir)
+    if os.name == "nt": executable_name = executable_name + '.EXE'
+    for directory in os.get_exec_path() + [js_dir]:
+        executable_path = os.path.join(directory, executable_name)
+        if os.path.isfile(executable_path) and os.access(executable_path, os.X_OK):
+           executable_path_list.append(executable_path)
+    if len(executable_path_list) == 0: return None
+    return executable_path_list[-1] # if multiple occourences
+
+def get_js_runtime(supported_runtimes):
+    for runtime in supported_runtimes:
+        js_runtime = find_executable(runtime)
+        if js_runtime: return js_runtime
+    return None
+
+g_js_runtime = get_js_runtime([ 'node', 'deno', 'bun'])
+
+def _run_js_runtime_bytes(js_file, requests, js_format="file"):
+    if not g_js_runtime:
+        raise NameError("No supported g_js_runtime is found!\nSupported runtimes: node.")
+
+    jscode = ""
+
+    if js_format == "file":
+        flag = 'rb'
+        jscode = read_preprocessed_js_file(js_file, flag, None)
+    elif js_format == "bytes":
+        flag = 'r'
+        jscode = read_preprocessed_js_file(js_file, flag, "utf-8")
+        jscode = re.sub(r'''(\[\{"type": "n", .+\}\])''', json.dumps(requests), jscode)
+        jscode = jscode.encode("utf-8")
+
+    cmd = [g_js_runtime, '-']
+    with Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as process:
+        stdout, stderr = process.communicate_or_kill(jscode)
+        if process.returncode == 0:
+            output = json.loads(stdout)
+        if process.returncode or stderr:
+            msg = 'Error running node process'
+            if stderr: msg = f'{msg}: {stderr}'
+            raise Exception(msg)
+
+    return output
+
+def validate_json(string):
+    try:
+        string = json.loads(string)
+    except ValueError as err:
+        raise Exception(f"Bad json: {err}")
+    return string
+
+def validate_response_type(output, response_type):
+    # check returned values
+    if response_type == None:
+        raise NotImplementedError('Request type not specified')
+    elif response_type == 'pass': # in case dont require or is different
+        pass
+
+def _run_js_runtime_file(js_file, *args, js_format="file", response_format='json', response_type=None):
+    if not g_js_runtime:
+        raise NameError("No supported g_js_runtime is found!\nSupported runtimes: node.")
+
+    cmd = [g_js_runtime, js_file, *args]
+    # process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # stdout, stderr = process.communicate()
+    with Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as process:
+        stdout, stderr = process.communicate_or_kill()
+        output = None
+        if process.returncode == 0 and not stderr:
+            output = stdout.decode()
+        elif process.returncode or stderr:
+            # output = stderr.decode()
+            msg = 'Error running node process'
+            if stderr: msg = f'{msg}: {stderr.decode()}'
+            raise Exception(msg)
+
+    # check if decoded string is json format
+    if response_format == 'json':
+        output = validate_json(output)
+        validate_response_type(output, response_type=response_type) # check if response correspond
+    else:
+        print(output)
+        raise NotImplementedError('Response format not implemented')
+
+    return output
 
 
 
