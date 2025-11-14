854debe9d34d586bed9a72c5c2a94e2d6fe7a6ae
ndgsa <59797540+ndgsa@users.noreply.github.com>
2025-11-14 18:34:47 +0200

    feature: display as grid youtube playlist

--- settings.py
+++ settings.py
@@ -309,6 +309,14 @@ Archive: https://archive.ph/OZQbN''',
         'category': 'interface',
     }),
 
+    ('display_as_grid_youtube', {
+        'label': 'Display youtube playlist videos as grid',
+        'type': bool,
+        'default': False,
+        'comment': '',
+        'category': 'interface',
+    }),
+
     ('subtitles_mode', {
         'type': int,
         'default': 0,
