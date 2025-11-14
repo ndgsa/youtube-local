54ca7706cf1bb40873bf07b13c1f5ca42da0bbc4
ndgsa <59797540+ndgsa@users.noreply.github.com>
2025-11-14 18:34:47 +0200

    search: get multiple pages at once, cache results
    - Request multiple pages.
    - Cache page result for 10 minutes.
    - Add page buttons at the top.

--- youtube/search.py
+++ youtube/search.py
@@ -9,6 +9,9 @@ import mimetypes
 from flask import request
 import flask
 import os
+import gevent
+import cachetools.func
+from math import ceil
 
 # Sort: 1
     # Upload date: 2
@@ -35,7 +38,14 @@ features = {
     'location': 23,
 }
 
+class HashableDict(dict):
+    def __hash__(self):
+        return hash(frozenset(self.items()))
+
+g_search_id_results = []
+
 def page_number_to_sp_parameter(page, autocorrect, sort, filters):
+    #offset = (int(page) - 1)*50    # 20 results per page
     offset = (int(page) - 1)*20    # 20 results per page
     autocorrect = proto.nested(8, proto.uint(1, 1 - int(autocorrect) ))
     filters_enc = proto.nested(2, proto.uint(1, filters['time']) + proto.uint(2, filters['type']) + proto.uint(3, filters['duration']))
@@ -63,19 +73,23 @@ def get_search_page():
     page = request.args.get("page", "1")
     autocorrect = int(request.args.get("autocorrect", "1"))
     sort = int(request.args.get("sort", "0"))
-    filters = {}
+    filters = HashableDict()
     filters['time'] = int(request.args.get("time", "0"))
     filters['type'] = int(request.args.get("type", "0"))
     filters['duration'] = int(request.args.get("duration", "0"))
-    polymer_json = get_search_json(query, page, autocorrect, sort, filters)
 
-    search_info = yt_data_extract.extract_search_info(polymer_json)
-    if search_info['error']:
-        return flask.render_template('error.html', error_message = search_info['error'])
+    page_multiplier = 1
 
-    for extract_item_info in search_info['items']:
-        util.prefix_urls(extract_item_info)
-        util.add_extra_html_info(extract_item_info)
+    if True:
+        search_info = get_many_pages_as_one(query, page, autocorrect, sort, filters, page_multiplier)
+    else:
+        polymer_json = get_search_json(query, page, autocorrect, sort, filters)
+        search_info = yt_data_extract.extract_search_info(polymer_json)
+        if search_info['error']:
+            return flask.render_template('error.html', error_message = search_info['error'])
+        for extract_item_info in search_info['items']:
+            util.prefix_urls(extract_item_info)
+            util.add_extra_html_info(extract_item_info)
 
     corrections = search_info['corrections']
     if corrections['type'] == 'did_you_mean':
@@ -137,4 +151,60 @@ def search_hidden_channels_hide(search_info_items):
         except Exception as e:
             print(e)
     return tmp[:]
+
+
+def get_search_json_and_extract(query, page, autocorrect, sort, filters):
+    polymer_json = get_search_json(query, page, autocorrect, sort, filters)
+    search_info = yt_data_extract.extract_search_info(polymer_json)
+    return search_info
+
+def joinall1(greenlets, timeout=None, raise_error=False, count=None):
+    done = []
+    for obj in gevent.wait(greenlets, timeout=timeout, count=count):
+        if getattr(obj, 'exception', None) is not None:
+            if hasattr(obj, '_raise_exception'):
+                obj._raise_exception()
+            else:
+                raise obj.exception
+        if obj.value == None: break
+        done.append(obj)
+    return done
+
+@cachetools.func.ttl_cache(maxsize=99, ttl=10*60)
+def get_many_pages_as_one(query, page, autocorrect, sort, filters, page_multiplier=3):
+    '''return a number of search results'''
+    pages_list = list(range((int(page) * page_multiplier) - page_multiplier + 1, (int(page) * page_multiplier) + 1))
+    search_info_tmp = {'error': None, 'estimated_results': 0, 'estimated_pages': 0, 'corrections': {'type': None}, 'items': []}
+
+    # if dont want to use greenlets
+    # for p in pages_list:
+        # polymer_json = get_search_json(query, p, autocorrect, sort, filters)
+        # search_info = yt_data_extract.extract_search_info(polymer_json)
+
+    tasks = []
+    for p in pages_list:
+        tasks.append(gevent.spawn(get_search_json_and_extract, query, p, autocorrect, sort, filters))
+    joinall1(tasks, raise_error=False)
+    util.check_gevent_exceptions(*tasks)
+
+    for t in tasks:
+        search_info = t.value
+        if search_info['error']:
+            return flask.render_template('error.html', error_message = search_info['error'])
+
+        for extract_item_info in search_info['items']:
+            util.prefix_urls(extract_item_info)
+            util.add_extra_html_info(extract_item_info)
+
+        corrections = search_info['corrections']
+        if corrections['type'] in ['did_you_mean', 'showing_results_for']:
+            return search_info
+
+        for k,v in search_info.items():
+            if k != 'items': search_info_tmp[k] = v
+            else: search_info_tmp['items'].extend(v)
+
+    search_info = search_info_tmp
+    search_info['estimated_pages'] = ceil(search_info['estimated_results']/(page_multiplier*20))
+    return search_info
 
