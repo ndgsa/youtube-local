834641edeef14ed7cf18fec056a821deff6a0fa7
ndgsa <59797540+ndgsa@users.noreply.github.com>
2026-02-05 19:28:45 +0200

    search: reverse order of pages
    - Calculate last page and reverse order of pages. (makes additional requsts)

--- innertube-client1/youtube/search.py
+++ innertube-client1/youtube/search.py
@@ -43,6 +43,7 @@ class HashableDict(dict):
         return hash(frozenset(self.items()))
 
 g_search_id_results = []
+g_last_page_list = []
 
 def page_number_to_sp_parameter(page, autocorrect, sort, filters):
     #offset = (int(page) - 1)*50    # 20 results per page
@@ -78,10 +79,15 @@ def get_search_page():
     filters['type'] = int(request.args.get("type", "0"))
     filters['duration'] = int(request.args.get("duration", "0"))
 
+    filters['reversed_order'] = request.args.get("reversed", None)
+
     page_multiplier = 1
 
     if True:
-        search_info = get_many_pages_as_one(query, page, autocorrect, sort, filters, page_multiplier)
+        if filters['reversed_order']:
+            search_info = get_many_pages_as_one_reversed(query, page, autocorrect, sort, filters, page_multiplier)
+        else:
+            search_info = get_many_pages_as_one(query, page, autocorrect, sort, filters, page_multiplier)
     else:
         polymer_json = get_search_json(query, page, autocorrect, sort, filters)
         search_info = yt_data_extract.extract_search_info(polymer_json)
@@ -102,7 +108,7 @@ def get_search_page():
         no_autocorrect_query_url = util.URL_ORIGIN + '/results?' + urllib.parse.urlencode(no_autocorrect_query_string, doseq=True)
         corrections['original_query_url'] = no_autocorrect_query_url
 
-    search_info['items'] = filter_search_items(search_info['items'][:], query, page, request.url, request.args, sort)
+    search_info['items'] = filter_search_items(search_info['items'][:], query, page, request.url, request.args, sort, filters)
 
     return flask.render_template('search.html',
         header_playlist_names = local_playlist.get_playlist_names(),
@@ -123,7 +129,7 @@ def get_search_engine_xml():
 
 
 
-def filter_search_items(search_info_items, query, page, request_url, request_args, sort):
+def filter_search_items(search_info_items, query, page, request_url, request_args, sort, filters):
     '''return filtered search items'''
     other_type_list = [item for item in search_info_items if item['type'] != 'video']
     # filter only video type items
@@ -207,4 +213,95 @@ def get_many_pages_as_one(query, page, autocorrect, sort, filters, page_multipli
     search_info = search_info_tmp
     search_info['estimated_pages'] = ceil(search_info['estimated_results']/(page_multiplier*20))
     return search_info
+
+
+@cachetools.func.ttl_cache(maxsize=99, ttl=10*60)
+def get_many_pages_as_one_reversed(query, page, autocorrect, sort, filters, page_multiplier=3):
+    '''return a number of search results'''
+    last_page, search_info = calculate_last_page(query, page, autocorrect, sort, filters)
+    search_info_tmp = {'error': None, 'estimated_results': 0, 'estimated_pages': 0, 'corrections': {'type': None}, 'items': []}
+    tasks = []
+
+    if (not last_page) or (not search_info): return search_info_tmp
+    elif search_info: estimated_results = search_info['estimated_results'] # store original estimated results
+
+    # calculate last page numbers
+    t_pages_list = list(range((int(page) * page_multiplier) - page_multiplier + 1, (int(page) * page_multiplier) + 1))
+    pages_list = []
+    for p in t_pages_list:
+        page_number = 1 + last_page - p
+        if page_number > 0:
+            pages_list.append(1 + last_page - p)
+        else: break
+
+    # in case search_info exist use it
+    if search_info and int(page) == 1:
+        try: pages_list.remove(last_page)
+        except ValueError: pass
+
+    for p in pages_list:
+        tasks.append(gevent.spawn(get_search_json_and_extract, query, p, autocorrect, sort, filters))
+    joinall1(tasks, raise_error=False)
+    util.check_gevent_exceptions(*tasks)
+
+    if search_info and int(page) == 1:
+        tasks.insert(0, search_info)
+
+    for t in range(len(tasks)):
+        if search_info and int(page) == 1 and t == 0 and isinstance(tasks[0], dict): search_info = tasks[0]
+        else: search_info = tasks[t].value
+
+        if search_info['error']:
+            return flask.render_template('error.html', error_message = search_info['error'])
+
+        for extract_item_info in search_info['items']:
+            util.prefix_urls(extract_item_info)
+            util.add_extra_html_info(extract_item_info)
+
+        for k,v in search_info.items():
+            if k != 'items': search_info_tmp[k] = v
+            else: search_info_tmp['items'].extend(v)
+
+    search_info = search_info_tmp
+    search_info['estimated_pages'] = ceil(last_page/page_multiplier)
+    search_info['estimated_results'] = estimated_results # last_page * 20
+    return search_info
+
+
+def calculate_last_page(query, page, autocorrect, sort, filters):
+    '''return last page number'''
+    global g_last_page_list # store params
+    last_page = None
+    search_info = None
+
+    # check if last_page already requested
+    for i in g_last_page_list:
+        if (query, sort, filters) == (i[0], i[1], i[2]):
+            if i[3] != None and i[4] != None: return i[3], i[4]
+            elif i[3] == None: g_last_page_list.remove(i) # remove params to make new request
+            break
+
+    search_info = get_search_json_and_extract(query, 1, autocorrect, sort, filters) # request first page
+    estimated_pages = ceil(search_info['estimated_results']/(20))
+
+    if estimated_pages == 0 or len(search_info) == 0: # stop requesting if nothing available
+        return None, None
+    elif estimated_pages == 1: # if 1 page available use it
+        last_page = estimated_pages
+    else:
+        retries = 2
+        while retries > 0:
+            search_info = get_search_json_and_extract(query, estimated_pages, autocorrect, sort, filters)
+            if len(search_info['items']) > 0:
+                last_page = estimated_pages # last page
+                break
+            if abs(ceil(search_info['estimated_results']/(20)) - estimated_pages) != 0:
+                estimated_pages = ceil(search_info['estimated_results']/(20))
+            else:
+                search_info = None # if same estimated_results
+                break
+            retries = retries - 1
+
+    g_last_page_list.append((query, sort, filters, last_page, search_info))
+    return last_page, search_info
 
