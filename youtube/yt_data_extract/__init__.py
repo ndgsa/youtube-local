9ebddeb9237468998fbd8d125d63d8017ff4f68a
ndgsa <59797540+ndgsa@users.noreply.github.com>
2026-05-28 11:44:43 +0300

    search: search api and refinement filters

--- youtube/yt_data_extract/__init__.py
+++ youtube/yt_data_extract/__init__.py
@@ -3,7 +3,7 @@ from .common import (get, multi_get, deep_get, multi_deep_get,
     extract_str, extract_formatted_text, extract_int, extract_approx_int,
     extract_date, extract_item_info, extract_items, extract_response)
 
-from .everything_else import (extract_channel_info, extract_search_info,
+from .everything_else import (extract_channel_info, extract_search_info, extract_search_refinement_info,
     extract_playlist_metadata, extract_playlist_info, extract_comments_info, check_for_empty_value)
 
 from .watch_extraction import (extract_watch_info, get_caption_url,
